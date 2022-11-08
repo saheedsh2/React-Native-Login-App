e8a890bd88e8c056aa8b93eba5663ffd5441e7b6
saheed <saheedsh2@gmail.com>
2022-11-08 12:47:49 +0100

    updated

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -3,12 +3,26 @@ import React from 'react'
 
 const CustomButton = () => {
   return (
-    <View>
-      <Text>CustomButton</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>CustomButton</Text>
     </View>
   )
 }
 
 export default CustomButton
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: 'green',
+        width: '100%',
+        padding: 15,
+        marginVertical: 5,
+        alignItems: 'center',
+        borderRadius: 5
+    },
+    text:{
+        fontWeight: 'bold',
+        color: 'white'
+    }
+
+})
